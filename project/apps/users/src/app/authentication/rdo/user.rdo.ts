25285159737c9205701c980b9b01b0f68d9c7a33
poluectov <poluectov@list.ru>
2023-08-12 13:54:15 +0300

    Добавил Response Data Object

---
+++ project/apps/users/src/app/authentication/rdo/user.rdo.ts
@@ -0,0 +1,25 @@
+import { Expose } from 'class-transformer';
+
+export class UserRdo {
+  @Expose({ name: '_id'})
+  public id: string;
+
+  @Expose()
+  public firstname: string;
+
+  @Expose()
+  public lastname: string;
+
+  @Expose()
+  public email: string;
+
+  @Expose()
+  public city: string;
+
+  @Expose()
+  public avatar: string;
+
+  @Expose()
+  public dateBirth: string;
+  
+}
