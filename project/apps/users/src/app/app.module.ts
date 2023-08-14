97a607de86167393b4c6786a44122656d242e2ec
poluectov <poluectov@list.ru>
2023-08-14 23:18:02 +0300

    Подключил библиотеку ConfigUsersModule в users/AppModule

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaskUserModule } from './task-user/task-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ConfigUsersModule } from '@project/config/config-users';
 
 @Module({
-  imports: [TaskUserModule, AuthenticationModule],
+  imports: [TaskUserModule, AuthenticationModule, ConfigUsersModule],
   controllers: [],
   providers: [],
 })
