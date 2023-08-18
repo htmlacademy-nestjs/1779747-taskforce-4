47f120927cb6c25e6a31d06b5878781c5429a98c
poluectov <poluectov@list.ru>
2023-08-18 23:34:37 +0300

    Добавил функцию getMongooseOptions

--- project/libs/config/config-users/src/index.ts
+++ project/libs/config/config-users/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/config-users.module';
-export { default as dbConfig } from './lib/db.config';
-export { default as appConfig } from './lib/app.config';
+export { default as dbConfig } from './lib/config/db.config';
+export { default as appConfig } from './lib/config/app.config';
+export * from './lib/get-mongoose-options';
 
 
