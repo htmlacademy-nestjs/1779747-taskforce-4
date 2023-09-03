45955c0059ed86974fb32c100c4dc66d22a356ef
poluectov <poluectov@list.ru>
2023-09-03 20:14:33 +0300

    Добавил контроллер FileController, file.module.ts, file.http и app.module.ts

--- project/apps/uploader/src/app/app.module.ts
+++ project/apps/uploader/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { FileModule } from './file/file.module';
+import { ConfigUploaderModule } from '@project/config/config-uploader';
+
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    FileModule,
+    ConfigUploaderModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
