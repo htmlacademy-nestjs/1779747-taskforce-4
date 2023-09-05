3a0fb613907403e0ca21df570395319a12e2e342
poluectov <poluectov@list.ru>
2023-09-06 02:04:26 +0300

    Добавил модуль Notify в сервис users

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { TaskUserModule } from './task-user/task-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { ConfigUsersModule, getMongooseOptions } from '@project/config/config-users';
 import { MongooseModule } from '@nestjs/mongoose';
+import { NotifyModule } from './notify/notify.module';
 
 @Module({
   imports: [
     AuthenticationModule,
     TaskUserModule,
     ConfigUsersModule,
+    NotifyModule,
     MongooseModule.forRootAsync(
       getMongooseOptions()
   )],
