f6e0264a8835a00faf25fe873f97af3dc2b157c9
poluectov <poluectov@list.ru>
2023-09-10 13:40:30 +0300

    Правки на защите

--- project/apps/notify/src/app/email-subscriber/dto/create-subscriber.dto.ts
+++ project/apps/notify/src/app/email-subscriber/dto/create-subscriber.dto.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
-import { EMAIL_NOT_VALID, FIRST_NAME_IS_EMPTY, USER_ID_IS_EMPTY } from '../email-subscriber.constant';
+import { EMAIL_NOT_VALID, FIRST_NAME_IS_EMPTY, USER_ID_IS_EMPTY, LAST_NAME_IS_EMPTY } from '../email-subscriber.constant';
 import { UserRole } from '@project/shared/app-types';
 
 export class CreateSubscriberDto {
@@ -9,7 +9,7 @@ export class CreateSubscriberDto {
   @IsNotEmpty({ message: FIRST_NAME_IS_EMPTY })
   public firstname: string;
 
-  @IsNotEmpty({ message: USER_ID_IS_EMPTY })
+  @IsNotEmpty({ message: LAST_NAME_IS_EMPTY })
   public lastname: string;
 
   public role: UserRole;
